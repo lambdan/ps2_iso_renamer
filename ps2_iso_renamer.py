331edec3f619ea0ee4f1a966f78e7affe93b4f91
djs <lambdan@users.noreply.github.com>
2019-08-24 02:56:00 +0200

    change to pcsx2 wiki id collection

--- ps2_iso_renamer.py
+++ ps2_iso_renamer.py
@@ -1,12 +1,12 @@
 import sys, os, re
-titles = 'games.csv'
+titles = 'ps2_ids.txt'
 keepcharacters = (' ','.','_','-')
 
 def gameTitleFromCSV(ID):
 	with open(titles) as t:
 		for line in t:
-			if line.startswith(ID):
-				return line.split(';')[1] # [0] = id, [1] = title
+			if ID in line:
+				return line.split(';')[0] # [0] = title, [1] = id's
 	
 second_chars = [b'C', b'L'] # SCxS, SLxS
 third_chars = [b'P', b'E', b'U'] # SxPS, SxES, SxUS
@@ -14,27 +14,37 @@ fourth_chars = [b'S', b'M']
 
 for file in os.listdir('.'):
 	if file.lower().endswith('iso'):
+		#print (file)
 		string = ""
 		with open(file, "rb") as f: # there is probably a *much* better way to do this
 			byte = f.read(1)
 			while byte:
 				if byte == b'S' and string == "":
 					string = byte.decode()
+					#print (string)
 					byte = f.read(1)
 					if byte in second_chars:
 						string += byte.decode()
+						#print (string)
 						byte = f.read(1)
 						if byte in third_chars:
 							string += byte.decode()
+							#print (string)
 							byte = f.read(1)
 							if byte in fourth_chars:
 								string += byte.decode()
-								byte = f.read(7)
-								string += byte.decode()
-								if "." in string:
-									break
-								else:
-									string = ""
+								#print (string)
+								byte = f.read(1)
+								if byte == b'_':
+									string += byte.decode()
+									#print (string)
+									byte = f.read(6)
+									string += byte.decode()
+									if "." in string:
+										#print (string)
+										break
+									else:
+										string = ""
 				else:
 					string = ""
 
@@ -44,9 +54,14 @@ for file in os.listdir('.'):
 		gameID = string.replace('_', '-')
 		gameID = gameID.replace('.', '')
 		title = gameTitleFromCSV(gameID)
-		cleanedTitle = "".join(c for c in title if c.isalnum() or c in keepcharacters).rstrip() # https://stackoverflow.com/a/7406369
-		cleanedTitle = cleanedTitle.replace('  ', ' ')
-		new_name = cleanedTitle + ' [' + gameID + '].iso'
+		if not title: # if title is not in the csv
+			print ('did not find title for', gameID)
+			print ('will use filename instead')
+			new_name = os.path.splitext(file)[0] + ' [' + gameID + '].iso'
+		else:
+			cleanedTitle = "".join(c for c in title if c.isalnum() or c in keepcharacters).rstrip() # https://stackoverflow.com/a/7406369
+			cleanedTitle = cleanedTitle.replace('  ', ' ')
+			new_name = cleanedTitle + ' [' + gameID + '].iso'
 
 		# rename
 		print (file, '--->', new_name)
