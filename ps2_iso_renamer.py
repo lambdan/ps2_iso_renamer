00c404b96019ff9a52b1c6eddab78a5b0fdf7954
djs <lambdan@users.noreply.github.com>
2019-08-24 19:11:10 +0200

    prettier ID finder

--- ps2_iso_renamer.py
+++ ps2_iso_renamer.py
@@ -1,62 +1,41 @@
 import sys, os, re
 titles = 'ps2_ids.txt'
-keepcharacters = (' ','.','_','-')
+keepcharacters = (' ','.','_','-') # Characters to keep when cleaning up the filename
 
 def gameTitleFromCSV(ID):
 	with open(titles) as t:
 		for line in t:
 			if ID in line:
 				return line.split(';')[0] # [0] = title, [1] = id's
-	
-second_chars = [b'C', b'L'] # SCxS, SLxS
-third_chars = [b'P', b'E', b'U'] # SxPS, SxES, SxUS
-fourth_chars = [b'S', b'M']
 
 for file in os.listdir('.'):
 	if file.lower().endswith('iso'):
 		#print (file)
-		string = ""
-		with open(file, "rb") as f: # there is probably a *much* better way to do this
+		bytelist = []
+		with open(file, "rb") as f:
 			byte = f.read(1)
 			while byte:
-				if byte == b'S' and string == "":
-					string = byte.decode()
-					#print (string)
-					byte = f.read(1)
-					if byte in second_chars:
-						string += byte.decode()
-						#print (string)
-						byte = f.read(1)
-						if byte in third_chars:
-							string += byte.decode()
+				
+				bytelist.append(byte)
+				if len(bytelist) == 11:  # SLPS-205.33 = 11 bytes 
+					if ord(bytelist[4]) == 95 and ord(bytelist[8]) == 46: # 5 char is _ and 9th is . (ascii)
+					#TODO: probably do more sanity checking to make sure first 4 chars are alpha etc
+						try:
+							string = b''.join(bytelist).decode('utf8')
 							#print (string)
-							byte = f.read(1)
-							if byte in fourth_chars:
-								string += byte.decode()
-								#print (string)
-								byte = f.read(1)
-								if byte == b'_':
-									string += byte.decode()
-									#print (string)
-									byte = f.read(6)
-									string += byte.decode()
-									if "." in string:
-										#print (string)
-										break
-									else:
-										string = ""
-				else:
-					string = ""
+							break
+						except UnicodeDecodeError:
+							continue
+					bytelist.pop(0) # remove first byte
 
 				byte = f.read(1)
 
 		# clean up filename and get title from .csv
-		gameID = string.replace('_', '-')
+		gameID = string.replace('_', '-') # these two lines converts SLUS_200.66 to SLUS-20066
 		gameID = gameID.replace('.', '')
 		title = gameTitleFromCSV(gameID)
 		if not title: # if title is not in the csv
-			print ('did not find title for', gameID)
-			print ('will use filename instead')
+			print ('did not find title for', gameID, ' -- using filename instead')
 			new_name = os.path.splitext(file)[0] + ' [' + gameID + '].iso'
 		else:
 			cleanedTitle = "".join(c for c in title if c.isalnum() or c in keepcharacters).rstrip() # https://stackoverflow.com/a/7406369
